6b8c90bca517ed29554803fe8c1614753185ec6a
Your Name <you@example.com>
2021-09-25 02:06:08 +0300

    file uploader

---
+++ vendor/2amigos/yii2-file-upload-widget/src/views/download.php
@@ -0,0 +1,49 @@
+<!-- The template to display files available for download -->
+<script id="template-download" type="text/x-tmpl">
+{% for (var i=0, file; file=o.files[i]; i++) { %}
+    <tr class="template-download fade{% if (file.type =="M") { %} main_photo{% } %}" id="photo_{%=file.id%}">
+        <td>
+            <span class="preview">
+                {% if (file.thumbnailUrl) { %}
+                    <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" data-gallery><img src="{%=file.thumbnailUrl%}"></a>
+                {% } %}
+            </span>
+        </td>
+        <td>
+            <p class="name">
+                {% if (file.url) { %}
+                    <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" {%=file.thumbnailUrl?'data-gallery':''%}>{%=file.name%}</a>
+                {% } else { %}
+                    <span>{%=file.name%}</span>
+                {% } %}
+            </p>
+            {% if (file.error) { %}
+                <div><span class="label label-danger"><?= Yii::t('fileupload', 'Error') ?></span> {%=file.error%}</div>
+            {% } %}
+        </td>
+        <td>
+            <span class="size">{%=o.formatFileSize(file.size)%}</span>
+        </td>
+        <td>
+            <?if (Yii::$app->controller->route == 'myactivity/update'):?>
+            <button class="btn btn-success set_main" data-photo-id={%=file.id%}>
+                <span>Сделать основным</span>
+            </button>
+            <?endif;?>
+            {% if (file.deleteUrl) { %}
+                <button class="btn btn-danger delete" data-type="{%=file.deleteType%}" data-url="{%=file.deleteUrl%}"{% if (file.deleteWithCredentials) { %} data-xhr-fields='{"withCredentials":true}'{% } %}>
+                    <i class="glyphicon glyphicon-trash"></i>
+                    <span><?= Yii::t('fileupload', 'Delete') ?></span>
+                </button>
+                <input type="checkbox" name="delete" value="1" class="toggle">
+            {% } else { %}
+                <button class="btn btn-warning cancel">
+                    <i class="glyphicon glyphicon-ban-circle"></i>
+                    <span><?= Yii::t('fileupload', 'Cancel') ?></span>
+                </button>
+            {% } %}
+        </td>
+    </tr>
+{% } %}
+
+</script>
